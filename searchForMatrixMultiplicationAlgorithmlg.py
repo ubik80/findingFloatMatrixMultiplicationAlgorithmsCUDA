8d779459eaacb26eb9f6ab77d457d01d8f9b1bdf
Till Spaeth <tillspaeth@web.de>
2024-08-20 21:01:34 +0200

    bug fixed

--- searchForMatrixMultiplicationAlgorithmlg.py
+++ searchForMatrixMultiplicationAlgorithmlg.py
@@ -1,11 +1,11 @@
 import sys
 import backpropCUDA as bp # pybind11 module in Cuda
 import numpy as np
+import time as tm
 
 dims = np.load("dims.npy") # load the dimensions of the problem to solve
 n = dims[0] # matrix dimensions of A and B
 p = dims[1] # number of products
-
 nn = n * n
 
 numOfBlocks = 46 # number of CUDA blocks to use
@@ -16,18 +16,19 @@ nueC = 0.1 # learning rate for Wc
 tol = 0.01 # tolerance for error in C (2-norm)
 maxNumOfIters = 3000000 # max number of iterations for backpropCUDA
 
-Wa = np.ndarray(p * nn, dtype=np.float32).reshape([p, nn]) # result is written here
-Wb = np.ndarray(p * nn, dtype=np.float32).reshape([p, nn])
-Wc = np.ndarray(p * nn, dtype=np.float32).reshape([nn, p])
-
 while True: # endlessly search for even better algorithms (lower p)
+    Wa = np.ndarray(p * nn, dtype=np.float32).reshape([p, nn])  # result is written here
+    Wb = np.ndarray(p * nn, dtype=np.float32).reshape([p, nn])
+    Wc = np.ndarray(p * nn, dtype=np.float32).reshape([nn, p])
+
     error = sys.float_info.max
-    iter_seed = 0;
+    iter = 0;
     while error > sys.float_info.max / 4.0: # while no solution found
-        iter_seed = iter_seed + 1
+        iter = iter + 1
+        seed = int((iter+tm.time()) % sys.maxsize)
         # calculate solution on GPU
         with np.errstate(over="ignore"):
-            error = bp.backpropCUDA(Wa, Wb, Wc, maxNumOfIters, nueAB, nueC, tol, iter_seed, numOfBlocks, numOfThreads)
+            error = bp.backpropCUDA(Wa, Wb, Wc, maxNumOfIters, nueAB, nueC, tol, seed, numOfBlocks, numOfThreads)
         # check if result is plausible
         if Wa.size < nn * p or Wb.size < nn * p or Wc.size < nn * p or error < 0.0:
             print (f"corrupted output, error {error}")
@@ -55,6 +56,3 @@ while True: # endlessly search for even better algorithms (lower p)
     np.save("dims", [n, p])
     np.save("solution_n" + str(n) + "_p" + str(p), [Wa, Wb, Wc.T]) # save solution
     p -= 1 # repeat with less products ...
-    Wa = np.ndarray(p * nn).reshape([p, nn])
-    Wb = np.ndarray(p * nn).reshape([p, nn])
-    Wc = np.ndarray(p * nn).reshape([nn, p])
